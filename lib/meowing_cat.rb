f29ac9fa5f558b419665f971b8f52cc97a9f7f0e
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-18 13:14:42 +0300

    Completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  # macro method that creates both the setter and getter methods
+  attr_accessor :name
+
+  #   instance method that prints meow! when called on the cat
+  def meow
+    puts "meow!"
+  end
+end
+
+# instantiates a new cat object
+maru = Cat.new
+# sets the cat name to Maru
+maru.name = "Maru"
+# displays meow! when called on the cat
+maru.meow
